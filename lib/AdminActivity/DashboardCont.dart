6988bc564eb4c4d5dc38930bbab6a4888db0f109
VishnuB01 <baasvishnu@gmail.com>
2024-07-05 13:53:10 +0530

    changes in LinearPercentIndicator in DashboardCont.dart file

--- lib/AdminActivity/DashboardCont.dart
+++ lib/AdminActivity/DashboardCont.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:screenshot/screenshot.dart';
 
 import '../EmpActivity/update_task.dart';
 
@@ -21,11 +22,6 @@ class DashboardCont extends StatelessWidget {
   final String taskType;
   final String taskdate;
   final String taskComp;
-  final List RandomImages = [
-    'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg',
-    'https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80',
-    'https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528'
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -36,15 +32,6 @@ class DashboardCont extends StatelessWidget {
       priColor = Color.fromARGB(167, 236, 233, 16);
     } else if (priority.compareTo("Low") == 0) {
       priColor = Color.fromARGB(167, 16, 236, 16);
-    }
-
-    var taskTypeColor;
-    if (taskType.compareTo("On Track") == 0) {
-      taskTypeColor = Color.fromARGB(137, 173, 71, 221);
-    } else if (taskType.compareTo("Meeting") == 0) {
-      taskTypeColor = Color.fromARGB(136, 73, 71, 221);
-    } else {
-      taskTypeColor = Color.fromARGB(255, 255, 0, 0);
     }
 
     return GestureDetector(
@@ -59,8 +46,8 @@ class DashboardCont extends StatelessWidget {
       child: Container(
         margin: EdgeInsets.fromLTRB(10, 30, 10, 0),
         padding: EdgeInsets.zero,
-        width: 200,
-        height: 150,
+        // width: 200,
+        // height: 150,
         decoration: BoxDecoration(
           shape: BoxShape.rectangle,
           color: Colors.white,
@@ -116,19 +103,17 @@ class DashboardCont extends StatelessWidget {
                       borderRadius: BorderRadius.circular(20.0),
                       border: Border.all(color: Color(0x4d9e9e9e), width: 1),
                     ),
-                    child: Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                        child: Text(
-                          priority,
-                          textAlign: TextAlign.center,
-                          overflow: TextOverflow.clip,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 14,
-                            color: Color(0xff000000),
-                          ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Text(
+                        priority,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.clip,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 14,
+                          color: Color(0xff000000),
                         ),
                       ),
                     ),
@@ -141,26 +126,24 @@ class DashboardCont extends StatelessWidget {
                     margin: EdgeInsets.zero,
                     padding: EdgeInsets.all(3),
                     constraints: const BoxConstraints(
-                      minHeight: 0,
-                      minWidth: 0,
-                      maxHeight: double.infinity,
-                      maxWidth: double.infinity,
+                      // minHeight: 0,
+                      // minWidth: 0,
+                      // maxHeight: double.infinity,
+                      // maxWidth: double.infinity,
                     ),
                     decoration: BoxDecoration(
                       shape: BoxShape.rectangle,
                       borderRadius: BorderRadius.circular(20.0),
                       border: Border.all(color: Color(0x4d9e9e9e), width: 1),
                     ),
-                    child: Expanded(
-                      child: Text(
-                        taskType,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
-                        ),
+                    child: Text(
+                      taskType,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 14,
+                        color: Color(0xff000000),
                       ),
                     ),
                   ),
@@ -168,9 +151,10 @@ class DashboardCont extends StatelessWidget {
               ],
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+              padding: EdgeInsets.fromLTRB(10, 20, 0, 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
                 children: [
                   Row(
                     children: [
@@ -181,22 +165,25 @@ class DashboardCont extends StatelessWidget {
                         padding: EdgeInsets.fromLTRB(3, 0, 0, 0),
                         child: Text(taskdate),
                       ),
-                      LinearPercentIndicator(
-                        width: 200,
-                        lineHeight: 10,
-                        curve: Curves.bounceIn,
-                        percent: int.tryParse(taskComp) != null
-                            ? int.parse(taskComp) / 100
-                            : 0.0,
-                        progressColor: Colors.blue,
-                      ),
+
                     ],
+                  ),
+                  Expanded(
+                    child: LinearPercentIndicator(
+                      // width:200,
+                      lineHeight: 10,
+                      curve: Curves.bounceIn,
+                      percent: int.tryParse(taskComp) != null
+                          ? int.parse(taskComp) / 100
+                          : 0.0,
+                      progressColor: Colors.blue,
+                    ),
                   ),
                   Padding(
                     padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
                     child: Row(
                       children: [
-                        Text(taskComp),
+                        Text('${taskComp}'),
                         const Icon(
                           Icons.percent_outlined,
                           size: 15,
--- lib/main.dart
+++ lib/main.dart
@@ -16,28 +16,40 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Task Management',
       theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         appBarTheme: AppBarTheme(
             backgroundColor: Colors.deepPurple,
             foregroundColor: Colors.white,
           elevation: 50,
         ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          elevation: 10,
-          backgroundColor: Colors.deepPurple,
-          foregroundColor: Colors.white,
-          splashColor: Colors.green,
-        ),
+        // floatingActionButtonTheme: FloatingActionButtonThemeData(
+        //   elevation: 10,
+        //   backgroundColor: Colors.deepPurple,
+        //   foregroundColor: Colors.white,
+        //   splashColor: Colors.green,
+        // ),
         cardTheme: CardTheme(
           elevation: 15,
         ),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+
+        // colorScheme: ColorScheme.dark(),
+        // colorSchemeSeed: const Color.fromRGBO(86, 80, 14, 171),
         primaryColor: Colors.green,
         fontFamily: 'Georgia',
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+
       ),
+      // theme: ThemeData(
+      //   colorSchemeSeed: const Color.fromRGBO(86, 80, 14, 171),
+      //   useMaterial3: true,
+      // ),
+
+
       home: LoginPage(), // Set LoginPage as the initial screen
     );
   }
